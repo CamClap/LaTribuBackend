22c0333166d3b172826f1faec411a485bb024490
CamClap <c.clappaz@hotmail.fr>
2025-06-13 13:36:26 +0200

    JWT Token OK, update and create user OK

---
+++ symfony/src/State/UserPasswordHasher.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProcessorInterface;
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\ORM\EntityManagerInterface;
+
+class UserPasswordHasher implements ProcessorInterface
+{
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher,
+        private EntityManagerInterface $em,
+        private ProcessorInterface $processor,
+    ) {}
+
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): User
+    {
+        if (!$data instanceof User) {
+            return $data;
+        }
+
+        // Hash le mot de passe s'il est fourni
+        if ($data->getPassword()) {
+            $hashedPassword = $this->passwordHasher->hashPassword($data, $data->getPassword());
+            $data->setPassword($hashedPassword);
+        }
+
+        $this->em->persist($data);
+        $this->em->flush();
+
+        return $data;
+    }
+}
